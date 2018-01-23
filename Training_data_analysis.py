804cb2913959fc5af92dd3f720b30826c7c125c3
Abraham Polk <abepolk@gmail.com>
2018-01-23 14:29:42 -0500

    Started to run a regression on level 1 categories

--- Training_data_analysis.py
+++ Training_data_analysis.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[164]:
+# In[1]:
 
 import pandas as pd
 import numpy as np
@@ -15,14 +15,14 @@ print(pd.__version__)
 get_ipython().magic('matplotlib inline')
 
 
-# In[165]:
+# In[2]:
 
 # Read table
 
 train_df = pd.read_table("./train.tsv", index_col = 'train_id', dtype = {'item_condition_id':CategoricalDtype(categories = [str(i) for i in range(1,6)], ordered = True), 'category_name':'category', 'brand_name': 'category', 'shipping':'category'})
 
 
-# In[166]:
+# In[3]:
 
 # Create columns for the first three hierarchical levels represented in the category name column.
 # Levels greater than one contain the names of the parent levels with the current level, separeated by slashes
@@ -38,7 +38,7 @@ for col in ['category_oneLevel', 'category_twoLevel', 'category_threeLevel']:
     train_df[col] = train_df[col].astype('category')
 
 
-# In[167]:
+# In[4]:
 
 # The categories of the first category level
 
@@ -46,7 +46,7 @@ for i in train_df.category_oneLevel.cat.categories.values:
     print(i)
 
 
-# In[168]:
+# In[5]:
 
 # The path of each category in the second level
 
@@ -54,13 +54,13 @@ for i in train_df.category_twoLevel.cat.categories.values:
     print(i)
 
 
-# In[169]:
+# In[6]:
 
 # The first few rows of the training data
 train_df.head()
 
 
-# In[170]:
+# In[7]:
 
 # Create column for (adult) clothing type that does not distinguish gender
 # NaN's stand for all the items that are not clothing, or have no category (but could be clothing anyway)
@@ -76,21 +76,21 @@ def get_clothing_type(elem):
 train_df['clothing_type'] = train_df.apply(get_clothing_type, axis = 1).astype('category')
 
 
-# In[171]:
+# In[8]:
 
 # The first few rows of the training data with the new transformed columns
 
 train_df.head(20)
 
 
-# In[172]:
+# In[9]:
 
 # Basic statistics about price, the only numerical category in the data
 
 train_df.describe()
 
 
-# In[173]:
+# In[10]:
 
 # A logarithmically scaled histogram of the price distribution
 
@@ -100,7 +100,7 @@ plt.xscale('log')
 plt.show() 
 
 
-# In[174]:
+# In[11]:
 
 # This probability plot tests whether log-transformed prices are normally distributed.
 # They seem to be heavy-tailed on the right, which is a pattern that mirrors pricing in financial markets (leptokurtosis)
@@ -108,13 +108,13 @@ plt.show()
 stats.probplot(np.log(train_df.price[train_df.price > 0]), dist = 'norm', plot = plt)
 
 
-# In[175]:
+# In[12]:
 
 # The number of items with each item condition category
 train_df.item_condition_id.value_counts()
 
 
-# In[176]:
+# In[13]:
 
 # A bar plot of the number of items with each item condition category, graphical representation of the above cell
 # I think pandas has a simplier method for a categorical series object, I'm not sure I needed to set this up myself in matplotlib
@@ -126,7 +126,7 @@ plt.ylabel('Number of items')
 plt.xlabel('Item condition number')
 
 
-# In[228]:
+# In[33]:
 
 # The next goal is to figure out, within each category, and then as a whole, the proportion of the error that can be eliminated by specifying the category.
 # Based on intuition from visual inspection of the names of the categories, it seems that specifying the second level category plausibly eliminates much of the error.
@@ -162,25 +162,65 @@ new_df = train_df.drop('category_twoLevel', axis = 1).assign(category_twoLevel =
 new_df = new_df.assign(log_price = np.log(new_df.price + 1))
 
 # Log price stats for whole dataset
-price_stats = pd.Series({'mean' : new_df.price.mean(), 'mean log': new_df.log_price.mean(), 'std log' : new_df.log_price.std(), 'sem log' : new_df.log_price.std()/new_df.log_price.count()})
+price_stats = pd.Series({'n' : new_df.price.count(), 'mean' : new_df.price.mean(), 'mean log': new_df.log_price.mean(), 'std log' : new_df.log_price.std(), 'sem log' : new_df.log_price.std()/np.sqrt(new_df.log_price.count())})
 #price_stats = get_group_stats(train_df.price)
 
 # Log price stats for each category - must add transform
 grouped_df = new_df.groupby('category_twoLevel')
-stats_dict = {'mean' : grouped_df.price.mean(), 'mean log': grouped_df.log_price.mean(), 'std log' : grouped_df.log_price.std(), 'sem log' : grouped_df.log_price.std()/grouped_df.log_price.count()}
+stats_dict = {'n' : grouped_df.price.count(), 'mean' : grouped_df.price.mean(), 'mean log': grouped_df.log_price.mean(), 'std log' : grouped_df.log_price.std(), 'sem log' : grouped_df.log_price.std()/np.sqrt(grouped_df.log_price.count())}
 category2_price_stats = pd.DataFrame(stats_dict)
 #category2_price_stats = get_group_stats(train_df.drop('category_twoLevel', axis = 1).assign(category_twoLevel = train_df.category_twoLevel.cat.add_categories('NaN').fillna('NaN')).groupby('category_twoLevel').price)
 
-print(price_stats)
-print(category2_price_stats)
+
+# In[34]:
+
+price_stats
 
 
-# In[230]:
+# In[35]:
 
-# Planning to regress price on the category names
+category2_price_stats
+
+
+# In[36]:
+
+# 
+
+
+# In[39]:
+
+# Planning to regress log price on the category names
 # This needs to be changed to log price as is done with the rest of this notebook
-result = sm.ols(formula = 'log_price ~ category_twoLevel', data = new_df).fit()
-result.summary()
+cat_one_results = sm.ols('log_price ~ category_oneLevel', data = new_df).fit()
+cat_one_results.summary()
+
+
+# In[49]:
+
+cat_one_results.params
+
+
+# In[50]:
+
+cat_one_results.bse
+
+
+# In[58]:
+
+# Assuming Gaussian distrubution (not completely sound, as shown by prior probability plot),
+# Find the KL divergences for each category
+cat_one_means = pd.Series(cat_one_results.params[0] + cat_one_results.params[1:])
+#{'mean' : cat_one_results.params)
+cat_one_sems = pd.Series(cat_one_results.bse[1:])
+cat_one_stds = cat_one_sems
+
+
+# In[56]:
+
+# Divergences of distributions of means
+
+
+#You want stds not sems
 
 
 # In[ ]:
